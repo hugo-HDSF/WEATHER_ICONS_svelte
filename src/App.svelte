513702f5d4d0f8cfc3bfd62a92b957bd888e4e42
hugo-HDSF <hugo.dasilva.filipe@gmail.com>
2023-07-19 12:26:16 +0200

    Forcast each 3h added

--- src/App.svelte
+++ src/App.svelte
@@ -30,8 +30,10 @@
     let loading = false;
 
     //weather data to be displayed
-    let current;
-    let forecasts;
+    let currentWeather;
+    let forecastWeather;
+    let currentWeatherData;
+    let forecastWeatherData;
 
     // Create a writable store to hold the weather data
     //(forcast as iterable array of objects)
@@ -43,8 +45,8 @@
 
     //subscribe to the weather data store changes
     weatherData.subscribe(value => {
-        current = value.current;
-        forecasts = value.forecasts;
+        currentWeather = value.current;
+        forecastWeather = value.forecasts;
     });
 
     //fetch weather data from the openweathermap API
@@ -59,22 +61,23 @@
         try {
             // Fetch the data from the API
             const currentResponse = await fetch(currentWeatherUrl);
-            const currentData = await currentResponse.json();
+            currentWeatherData = await currentResponse.json();
 
             const forecastResponse = await fetch(forecastWeatherUrl);
-            const forecastData = await forecastResponse.json();
+            forecastWeatherData = await forecastResponse.json();
 
             weatherData.set({
                 current: {
                     // map the current weather data to convert the object of arrays into an array of objects (iterable for #each)
-                    temp_min: currentData.main.temp_min.toPrecision(2),
-                    temp_max: currentData.main.temp_max.toPrecision(2),
-                    weather: currentData.weather[0].description,
-                    dt: new Date(currentData.dt * 1000),
-                    icon: currentData.weather[0].icon,
+                    temp_min: currentWeatherData.main.temp_min.toPrecision(2),
+                    temp_max: currentWeatherData.main.temp_max.toPrecision(2),
+                    temp: currentWeatherData.main.temp.toPrecision(2),
+                    weather: currentWeatherData.weather[0].description,
+                    dt: new Date(currentWeatherData.dt * 1000),
+                    icon: currentWeatherData.weather[0].icon,
                 },
                 forecasts: Object.entries(
-                    forecastData.list
+                    forecastWeatherData.list
                         // Filter out the 3h period forecasts for the current date (we only want the next 5 days)
                         .filter(_3hforecast => {
                             let currentDate = new Date().setHours(0, 0, 0, 0);
@@ -91,28 +94,34 @@
                             return acc;
                         }, {})
                     // map the forecasts for the 5 next days to convert the object of arrays into an array of objects (iterable for #each)
-                ).map(([date, forecasts]) => {
+                ).map(([date, forecast]) => {
+                    // if the last array has less than 4 forecasts, get the last element, else, get the 4th (3h * [4] = 12h noon)
+                    let forecastIndex = forecast.length < 4 ? forecast.length - 1 : 3;
                     return {
                         date,
                         //get the min and max temp for the day
-                        temp_min: Math.min(...forecasts.map(forecast => parseFloat(forecast.main.temp_min.toPrecision(2)))),
-                        temp_max: Math.max(...forecasts.map(forecast => parseFloat(forecast.main.temp_max.toPrecision(2)))),
+                        temp_min: Math.min(...forecast.map(forecast => parseFloat(forecast.main.temp_min.toPrecision(2)))),
+                        temp_max: Math.max(...forecast.map(forecast => parseFloat(forecast.main.temp_max.toPrecision(2)))),
+                        temp: forecast[forecastIndex].main.temp.toPrecision(2),
                         //get the 4th weather icon for the day => (3h * 4 ) = 12h (noon) (show the weather at noon, usually the warmest time of the day and the most relevant)
-                        icon: forecasts[4].weather[0].icon,
-                        dt: new Date(forecasts[0].dt * 1000),
+                        icon: forecast[forecastIndex].weather[0].icon,
+                        dt: new Date(forecast[0].dt * 1000),
                     }
                 })
 
             });
             //set loading state to false
             loading = false;
+            currentWeatherData = currentWeatherData.list;
+            forecastWeatherData = forecastWeatherData.list;
 
             console.log('Weather data fetched successfully');
-            console.log('Current weather data:', currentData);
-            console.log('Forecast weather data:', forecastData);
 
-            console.log('Current weather data:', current);
-            console.log('Forecasts weather data:', forecasts);
+            //console.log('Current weather data:', currentData);
+            console.log('Forecast weather data:', forecastWeatherData);
+
+            //console.log('Current weather mapped:', current);
+            //console.log('Forecasts weather mapped:', forecasts);
 
         } catch
             (error) {
@@ -133,9 +142,13 @@
 
     //date formatting for ->
     // - current weather
-    const current_weather_options = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
-    // - forecast weather
-    const forcast_weather_options = {weekday: 'short'};
+    const current_weather_fulldate_option = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
+    // - forecast weekday
+    const forecast_weather_weekday_option = {weekday: 'short'};
+    // - forecast 3h hour
+    const forecast3h_weather_time_option = {hour: 'numeric', hour12: false };
+    // - forecast day/month
+    const forecast3h_weather_date_options = {day: 'numeric', month: 'numeric'};
 
 </script>
 
@@ -154,6 +167,8 @@
                 </select>
             </label>
         </div>
+
+
         {#if loading}
             <div class="wrapper">
                 <div class="lds-spinner">
@@ -171,43 +186,78 @@
                     <div></div>
                 </div>
             </div>
-        {:else if current}
+
+
+        {:else if currentWeather}
+            <!-- today's current weather -->
             <div class="current-weather">
-                <h2>{current.dt.toLocaleDateString('fr-FR', current_weather_options)}</h2>
+                <h2>{currentWeather.dt.toLocaleDateString('fr-FR', current_weather_fulldate_option)}</h2>
                 <h1>{city.name}</h1>
-                <span class="main-temp">{current.temp_max}°</span>
-                <span class="weather-state">{current.weather}</span>
+                <span class="main-temp">{currentWeather.temp}°</span>
+                <span class="weather-state">{currentWeather.weather}</span>
                 <div class="temps">
                     <i class="high-temp"></i>
-                    <p>{current.temp_max}°</p>
+                    <p>{currentWeather.temp_max}°</p>
                     <span></span>
                     <i class="low-temp"></i>
-                    <p>{current.temp_min}°</p>
+                    <p>{currentWeather.temp_min}°</p>
                 </div>
             </div>
+
+
+            <!-- forecasts each 3h -->
+            <div class="card">
+                <div class="title">
+                    <svg class="calendar"></svg>
+                    <span>prévisions toutes les 3 heures</span>
+                </div>
+                <div class="forecasts3h">
+                    <!-- rightnow's forecast -->
+                    <div class="forecast3h">
+                        <p>{currentWeather.dt.toLocaleDateString('fr-FR',forecast3h_weather_date_options)}</p>
+                        <p>Maint.</p>
+                        <p><img class="weather-icon" src="images/{currentWeather.icon}.svg" alt=""></p>
+                        <p>{currentWeather.temp}°</p>
+                    </div>
+                    <!-- next 4 days forecast each 3h -->
+                    {#each forecastWeatherData as forecast3h}
+                        <div class="forecast3h">
+                            <p>{new Date(forecast3h.dt * 1000).toLocaleDateString('fr-FR',forecast3h_weather_date_options)}</p>
+                            <p>{new Date(forecast3h.dt * 1000).toLocaleTimeString('fr-FR',forecast3h_weather_time_option)}</p>
+                            <p><img class="weather-icon" src="images/{forecast3h.weather[0].icon}.svg" alt=""></p>
+                            <p>{forecast3h.main.temp.toPrecision(2)}°</p>
+                        </div>
+                    {/each}
+                </div>
+            </div>
+
+
+            <!-- forecasts for the next 5 days -->
             <div class="card">
                 <div class="title">
                     <svg class="calendar"></svg>
                     <span>prévisions sur 5 jours</span>
                 </div>
+                <!-- today's forecast -->
                 <div class="forecast">
                     <div class="forecast-day">
                         <p>Auj.</p>
                     </div>
                     <div class="forecast-weather">
                         <p>Maint.</p>
-                        <p><img class="weather-icon" src="images/{current.icon}.svg" alt=""></p>
+                        <p><img class="weather-icon" src="images/{currentWeather.icon}.svg" alt=""></p>
                     </div>
                     <div class="forecast-temps">
-                        <p>{current.temp_min}°</p>
+                        <p>{currentWeather.temp_min}°</p>
                         <span></span>
-                        <p>{current.temp_max}°</p>
+                        <p>{currentWeather.temp_max}°</p>
                     </div>
                 </div>
-                {#each forecasts as forecast}
+                <!-- next 4 days forecast -->
+                {#each forecastWeather as forecast}
                     <div class="forecast">
                         <div class="forecast-day">
-                            <p>{forecast.dt.toLocaleDateString('fr-FR', forcast_weather_options)}</p>
+                            <p>{forecast.dt.toLocaleDateString('fr-FR', forecast_weather_weekday_option)}</p>
                         </div>
                         <div class="forecast-weather">
                             <p><img class="weather-icon" src="images/{forecast.icon}.svg" alt=""></p>
